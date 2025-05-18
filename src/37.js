57fbf3c1b514318f633722395a24f14afb6bc447
osterbergrettkowski490 <osterbergrettkowski490@gmail.com>
2025-05-18 11:12:49 +0000

    Add 37.js

---
+++ src/37.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
